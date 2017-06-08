560b490eeecf49bff4c9065f2001253e5e78a64d
ffalt <ffalty@gmail.com>
2017-06-08 09:52:21 +0200

    server/client may need different backendUrls

--- setup/config_frontend.js
+++ setup/config_frontend.js
@@ -2,7 +2,7 @@ let path = require('path');
 let settings = {
 	server: {
 		listen: {  // where the frontend should be listening
-			host: '127.0.0.1',
+			host: '0.0.0.0',
 			port: 3000
 		},
 		data: { // absolute paths to the data folders (see https://github.com/digiwhist/opentender-data)
